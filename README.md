ba7f42f90cadef4c37a598b165d8f3d355756d60
Iurii Bairamukov <bairamykov@gmail.com>
2021-02-09 21:31:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Стартовый шаблон 🚀CRA
+
+### Включает в себя:
+
+- styled-components
+- styled-normalize
+- babel-plugin-styled-components
+- gh-pages
+- готовую стартовую инфраструктуру
---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/macro';
+import GlobalStyle from '../../styles/globalStyles';
+
+function App() {
+  return (
+    <Wrapper>
+      <GlobalStyle />
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.div``;
