5c34d9129352dd0a37a41628572cfab595de210f
Iurii Bairamukov <juribai@mail.ru>
2021-02-11 11:49:09 +0700

    feat: настроить шрифт

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import styled from 'styled-components/macro';
 import GlobalStyle from '../../styles/globalStyles';
-
+import GlobalFonts from '../../vendor/fonts/fonts';
 function App() {
   return (
     <Wrapper>
       <GlobalStyle />
+      <GlobalFonts />
     </Wrapper>
   );
 }
 
 export default App;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  max-width: 1320px;
+  padding: 30px 0;
+  margin: 0 auto;
+`;
